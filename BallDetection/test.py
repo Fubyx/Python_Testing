578614093f306b7d18a1a3971ad011c2a42e5679
MyUsernameDDM <mayrdamian9@gmail.com>
2024-03-29 09:05:01 +0100

    Added Sobel Filter (Edgedetection) to the Balldetection test.py

--- BallDetection/test.py
+++ BallDetection/test.py
@@ -24,8 +24,30 @@ while True:
     # Convert the frame to grayscale and apply Gaussian blur
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     blurred = cv2.GaussianBlur(gray, (5, 5), 0)
+
+    #https://pyimagesearch.com/2021/05/12/image-gradients-with-opencv-sobel-and-scharr/
+    
+    # set the kernel size, depending on whether we are using the Sobel
+    # operator of the Scharr operator, then compute the gradients along
+    # the x and y axis, respectively
+    ksize = 3
+    gX = cv2.Sobel(gray, ddepth=cv2.CV_32F, dx=1, dy=0, ksize=ksize)
+    gY = cv2.Sobel(gray, ddepth=cv2.CV_32F, dx=0, dy=1, ksize=ksize)
+    # the gradient magnitude images are now of the floating point data
+    # type, so we need to take care to convert them back a to unsigned
+    # 8-bit integer representation so other OpenCV functions can operate
+    # on them and visualize them
+    gX = cv2.convertScaleAbs(gX)
+    gY = cv2.convertScaleAbs(gY)
+    # combine the gradient representations into a single image
+    combined = cv2.addWeighted(gX, 0.5, gY, 0.5, 0)
+    # show our output images
+    cv2.imshow("Sobel/Scharr X", gX)
+    cv2.imshow("Sobel/Scharr Y", gY)
+    cv2.imshow("Sobel/Scharr Combined", combined)
+
     # Detect circles using Hough transform
-    circles = cv2.HoughCircles(blurred, cv2.HOUGH_GRADIENT, 1, 50, param1=90, param2=30, minRadius=0, maxRadius=75)
+    circles = cv2.HoughCircles(combined, cv2.HOUGH_GRADIENT, 1, 50, param1=90, param2=30, minRadius=0, maxRadius=75)
     """
     #If circles are detected, draw a red circle around the largest one 
     if circles is not None:
