bc4821de57c021f8b510a5ad4a6eaef5c1c5b1ac
David Mairhofer <david2mairhofer@gmail.com>
2024-04-08 12:05:33 +0200

    idk

--- TestVideoStream/clientOnPiV2/server.py
+++ TestVideoStream/clientOnPiV2/server.py
@@ -1,10 +1,11 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, Response
 import cv2
 import threading
 import numpy as np
 
 app = Flask(__name__)
 frame = None  # Global variable to store the latest frame
+image_data = None
 
 def display_frame():
     global frame
@@ -27,6 +28,13 @@ def receive_frame():
     frame = cv2.imdecode(np.frombuffer(image_data, np.uint8), cv2.IMREAD_COLOR)  # Update global frame
 
     return jsonify({'message': 'Frame received successfully'}), 200
+
+@app.route('/img')
+def img():
+    return Response(b'--frame\r\n'
+                    b'Content-Type: text/plain\r\n\r\n'+
+                    image_data+b'\r\n',
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
